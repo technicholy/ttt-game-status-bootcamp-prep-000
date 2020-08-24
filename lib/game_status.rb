74e01693f9074fbd9f6cabb602fd68c01cb7426f
Clayton Pierce <bamer78@gmail.com>
2020-08-24 21:52:31 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def won(board)
     win_combination.each do |pos|
       moves << board[pos]
     end
-    ((moves == ["X","X","X"]) || (moves == ["O","O","O"])) ? return win_combination : return false
+    ((moves == ["X","X","X"]) || (moves == ["O","O","O"])) ? (return win_combination) : (return false)
   end
 end
 
