7d0e970b27ac7a2cf4b3ee5efd0ef63efbbcfbaa
Clayton Pierce <bamer78@gmail.com>
2020-08-24 21:23:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,23 +15,19 @@ WIN_COMBINATIONS = [
 ]
 def won(board)
   for each win_combination in WIN_COMBINATIONS
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1] # load the value of the board at win_index_1
-  position_2 = board[win_index_2] # load the value of the board at win_index_2
-  position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-  if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-    (position_1 == "O" && position_2 == "O" && position_3 == "O")
-    return win_combination # return the win_combination indexes that won.
-  else
-    false
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+      (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+    end
   end
-end
 end
 
 def full(board)
