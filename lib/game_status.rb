45405ba1861f6b69e9c8b3e013e2cb9200d89d72
Clayton Pierce <bamer78@gmail.com>
2020-08-24 22:13:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,7 +14,7 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 def won(board)
-  WIN_COMBINATIONS.each do |pos1, pos2, pos3| 
+  WIN_COMBINATIONS.each do |pos1, pos2, pos3|
     moves = [board[pos1],board[pos2],board[pos3]]
     ((moves == ["X","X","X"]) || (moves == ["O","O","O"])) ? (return [pos1, pos2, pos3]) : (return false)
   end
